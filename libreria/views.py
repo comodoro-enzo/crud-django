b5f3aa108365ee7d3bc7ac184995c69f0f6bb9f6
enzo <comodoroenzo@gmail.com>
2023-04-03 05:59:42 -0300

    creando las vistas del crud

--- libreria/views.py
+++ libreria/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("<h1>Privet</h1>")
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    return render(request, 'libros/index.html')
